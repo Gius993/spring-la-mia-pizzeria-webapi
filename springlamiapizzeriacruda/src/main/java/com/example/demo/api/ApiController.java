c21f04d339be1e3a449ea6fd777b21a5fc9654b5
Gius993 <giuseppevidili9@gmail.com>
2023-02-24 16:23:05 +0100

    api controller

---
+++ springlamiapizzeriacruda/src/main/java/com/example/demo/api/ApiController.java
@@ -0,0 +1,64 @@
+package com.example.demo.api;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Pizza;
+import com.example.demo.repo.Pizzarepo;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/pizze")
+public class ApiController {
+	@Autowired
+	 Pizzarepo pizzarepo;
+	
+	
+
+	@GetMapping()		
+	public List<Pizza> index() {
+		return pizzarepo.findAll();
+	}
+	
+	@GetMapping("/{id}")		
+	public ResponseEntity<Pizza> detail(@PathVariable("id") Integer id) {
+		Optional<Pizza> res=pizzarepo.findById(id);
+		if (res.isPresent()) 
+			return new ResponseEntity<Pizza>(res.get(), HttpStatus.OK);
+	    else
+	    	return new ResponseEntity<Pizza>(HttpStatus.NOT_FOUND);
+	}
+	
+	@PostMapping("/create")
+	public Pizza create(@RequestBody Pizza pizza) {
+		return pizzarepo.save(pizza);
+	}	
+	@PutMapping("/{id}")	
+	public Pizza update(@RequestBody Pizza pizza,
+			@PathVariable("id") Integer id) {
+		Pizza p=pizzarepo.getReferenceById(id);
+		p.setName(pizza.getName());
+		//....
+		return pizzarepo.save(p);
+	}
+	
+	@DeleteMapping("/{id}")	
+	public void delete(
+			@PathVariable("id") Integer id) {
+		pizzarepo.deleteById(id);
+	}
+		
+}
--- springlamiapizzeriacruda/src/main/java/com/example/demo/controller/PizzaController.java
+++ springlamiapizzeriacruda/src/main/java/com/example/demo/controller/PizzaController.java
@@ -2,72 +2,103 @@ package com.example.demo.controller;
 
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
-import org.springframework.web.bind.annotation.RestController;
-
+import com.example.demo.model.Ingredienti;
 import com.example.demo.model.Pizza;
-
+import com.example.demo.repo.Ingredientirepo;
 import com.example.demo.repo.Pizzarepo;
 
+import jakarta.validation.Valid;
 
 
 
-
-@RestController
-@CrossOrigin
-
+@Controller
+@RequestMapping("/")
 public class PizzaController {
 	@Autowired
 	 Pizzarepo pizzarepo;
-	
-	
-
-	@GetMapping("/api")		
-	public List<Pizza> index() {
-		return pizzarepo.findAll();
+	@Autowired
+	private Ingredientirepo ingredientirepo;
+	@GetMapping
+	public String index(Model model, @RequestParam(name = "keyword", required = false) String keyword) {
+		List<Pizza> listaPizze;
+		
+		
+		if(keyword == null) {
+			listaPizze = pizzarepo.findAll();
+		}else {
+			listaPizze = pizzarepo.findByNameLike("%"+keyword+"%"); 
+		}
+		model.addAttribute("pizze", listaPizze);
+		return "pizze";
 	}
 	
-	@GetMapping("/{id}")		
-	public ResponseEntity<Pizza> detail(@PathVariable("id") Integer id) {
-		Optional<Pizza> res=pizzarepo.findById(id);
-		if (res.isPresent()) 
-			return new ResponseEntity<Pizza>(res.get(), HttpStatus.OK);
-	    else
-	    	return new ResponseEntity<Pizza>(HttpStatus.NOT_FOUND);
+	@GetMapping("/details/{id}")
+	public String details(@PathVariable("id") Integer id,Model model) {
+		Pizza pizza = pizzarepo.getReferenceById(id);
+		model.addAttribute("pizza", pizza);
+		return "details";
 	}
 	
+	@GetMapping("/create")
+	public String create(Model model) {
+		Pizza pizza = new Pizza();
+		List<Ingredienti> ingredienti = ingredientirepo.findAll(Sort.by("name"));
+		model.addAttribute("ingredienti", ingredienti);
+		model.addAttribute("pizza", pizza);
+		return "create";
+	}
 	@PostMapping("/create")
-	public Pizza create(@RequestBody Pizza pizza) {
-		return pizzarepo.save(pizza);
+	public String store(
+		@Valid	@ModelAttribute("pizza") Pizza formPizza,
+		BindingResult bindingResult,
+		Model model) {
+		if(bindingResult.hasErrors()) 
+			return "create";
+		
+		pizzarepo.save(formPizza);
+		return "redirect:/";
 	}	
-	@PutMapping("/{id}")	
-	public Pizza update(@RequestBody Pizza pizza,
-			@PathVariable("id") Integer id) {
-		Pizza p=pizzarepo.getReferenceById(id);
-		p.setName(pizza.getName());
-		//....
-		return pizzarepo.save(p);
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") Integer id,Model model) {
+		Pizza pizza = pizzarepo.getReferenceById(id);
+		model.addAttribute("pizza", pizza);
+		List<Ingredienti> ingredienti = ingredientirepo.findAll(Sort.by("name"));
+		model.addAttribute("ingredienti", ingredienti);
+		return "edit";
+	}
+	@PostMapping("/edit/{id}")		
+	public String update(
+			@Valid @ModelAttribute Pizza formPizza,
+			BindingResult bindingResult,
+			Model model) {
+		
+		if (bindingResult.hasErrors())
+			return "edit";
+		
+		pizzarepo.save(formPizza);
+		
+		return "redirect:/";
 	}
 	
-	@DeleteMapping("/{id}")	
-	public void delete(
-			@PathVariable("id") Integer id) {
-		pizzarepo.deleteById(id);
+	@PostMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Integer id) {
+	 
+	   pizzarepo.deleteById(id);
+	   
+	   return "redirect:/";
 	}
 			
 }
