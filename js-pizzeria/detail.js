c3a999e81b8fd445ea8a98f944d7839e8ef094be
Gius993 <giuseppevidili9@gmail.com>
2023-02-27 15:03:10 +0100

    index e dettaglio front office

---
+++ js-pizzeria/detail.js
@@ -0,0 +1,16 @@
+
+const URLParams=new URLSearchParams(window.location.search);
+const pizzeId=URLParams.get('id');
+
+axios.get(`http://localhost:8080/api/pizze/${pizzeId}`).then((list) => {
+    console.log( list);
+   
+    document.querySelector('#name').innerHTML= list.data.name;
+    document.querySelector('#description').innerHTML= list.data.description;
+    document.querySelector('#img').innerHTML= list.data.img;
+    document.querySelector('#price').innerHTML= list.data.price;
+    
+}).catch((list) =>{
+	console.error("non si puo eseguire la pagina riprova dopo" , list);
+	alert('Errore durante la connessione');
+	}); 
---
+++ js-pizzeria/index.js
@@ -0,0 +1,14 @@
+
+function pizzeList(){
+	axios.get("http://localhost:8080/api/pizze").then((list) =>{
+		console.log(list);
+		document.getElementById("pizze_list").innerHTML = '';
+		list.data.forEach(pizza => {
+		document.getElementById("pizze_list").innerHTML += `<li><a href="detail.html?id=${pizza.id}">${pizza.name}</a></li> `;
+		});
+	}).catch((list) =>{
+	console.error("non si puo eseguire la pagina riprova dopo" , list);
+	alert('Errore durante la connessione');
+	}); 
+}
+pizzeList();
